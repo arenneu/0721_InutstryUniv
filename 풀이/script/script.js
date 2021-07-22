e543db0a30172a5ba8d1f024a61c47790a59889a
arenneu <jisusjy@naver.com>
2021-07-22 14:45:39 +0900

    gnb_메뉴완료

--- 풀이/script/script.js
+++ 풀이/script/script.js
@@ -1,6 +1,14 @@
 $(function () {
     // gnb 메뉴
+    $(".mainLi, #subMenuBG").mouseover(function(){
+        $(".subMenu").stop().slideDown(500);
+        $("div#subMenuBG").stop().slideDown(500);
+    });
 
+    $(".mainLi, #subMenuBG").mouseout(function(){
+        $(".subMenu").stop().slideUp(50);
+        $("div#subMenuBG").stop().slideUp(50);
+    });
 
 
 
