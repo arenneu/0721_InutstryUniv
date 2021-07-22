91094b5bede476c657c00df5bdf486b01355fabd
arenneu <jisusjy@naver.com>
2021-07-22 15:29:05 +0900

    슬라이드쇼_구현완료

--- 풀이/script/script.js
+++ 풀이/script/script.js
@@ -1,5 +1,5 @@
 $(function () {
-    // gnb 메뉴
+    // gnb 메뉴 시작
     $(".mainLi, #subMenuBG").mouseover(function(){
         $(".subMenu").stop().slideDown(500);
         $("div#subMenuBG").stop().slideDown(500);
@@ -9,6 +9,20 @@ $(function () {
         $(".subMenu").stop().slideUp(50);
         $("div#subMenuBG").stop().slideUp(50);
     });
+    // gnb 메뉴 끝
+    // 슬라이드쇼
+
+    setInterval(fnslide, 3000);
+    function fnslide(){
+        $("#shuttleFrame").animate(
+            {"margin-left":"-1200px"},
+            3000,
+            function(){
+                $("#slide a:first-child").insertAfter("#slide a:last-child");
+                $("#shuttleFrame").css({"margin-left":"0"});
+            }
+        );
+    }
 
 
 
